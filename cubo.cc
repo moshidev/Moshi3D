30a3ea351d709e3ee4a36bc66af5acf3d914ec4d
Daniel P <46326833+moshidev@users.noreply.github.com>
2021-10-08 14:12:11 +0200

    refactor constructor

--- cubo.cc
+++ cubo.cc
@@ -4,28 +4,31 @@
 
 Cubo::Cubo(float lado)
 {
-   float offset = lado/2;
-   
-   v.push_back({offset, offset, offset});
-   v.push_back({offset, offset, -offset});
-   v.push_back({offset, -offset, offset});
-   v.push_back({offset, -offset, -offset});
-   v.push_back({-offset, offset, offset});
-   v.push_back({-offset, offset, -offset});
-   v.push_back({-offset, -offset, offset});
-   v.push_back({-offset, -offset, -offset});
+   const float offset = lado/2;
 
-   f.push_back({0, 1, 2});
-   f.push_back({0, 2, 6});
-   f.push_back({0, 4, 5});
-   f.push_back({0, 5, 1});
-   f.push_back({0, 6, 4});
-   f.push_back({1, 3, 2});
-   f.push_back({1, 5, 7});
-   f.push_back({1, 7, 3});
-   f.push_back({2, 3, 6});
-   f.push_back({3, 7, 6});
-   f.push_back({4, 6, 7});
-   f.push_back({4, 7, 5});
+   vertices.reserve(8);
+   triangles.reserve(12);
+   
+   vertices.emplace_back(-offset, -offset, -offset);
+   vertices.emplace_back(-offset, -offset,  offset);
+   vertices.emplace_back(-offset,  offset, -offset);
+   vertices.emplace_back(-offset,  offset,  offset);
+   vertices.emplace_back(offset, -offset, -offset);
+   vertices.emplace_back(offset, -offset,  offset);
+   vertices.emplace_back(offset,  offset, -offset);
+   vertices.emplace_back(offset,  offset,  offset);
+
+   triangles.emplace_back(0, 1, 5);
+   triangles.emplace_back(0, 5, 4);
+   triangles.emplace_back(0, 2, 3);
+   triangles.emplace_back(0, 3, 1);
+   triangles.emplace_back(0, 4, 2);
+   triangles.emplace_back(1, 3, 7);
+   triangles.emplace_back(1, 5, 7);
+   triangles.emplace_back(6, 5, 7);
+   triangles.emplace_back(6, 4, 5);
+   triangles.emplace_back(6, 2, 4);
+   triangles.emplace_back(6, 2, 3);
+   triangles.emplace_back(6, 7, 3);
 }
 
