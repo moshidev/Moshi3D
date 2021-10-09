6ecfd70ba2f199c36836bcb49dc2c0d88ac9f4e5
Daniel P <46326833+moshidev@users.noreply.github.com>
2021-10-09 12:47:16 +0200

    fix indices order

--- cubo.cc
+++ cubo.cc
@@ -18,17 +18,17 @@ Cubo::Cubo(float lado)
    vertices.emplace_back(offset,  offset, -offset);
    vertices.emplace_back(offset,  offset,  offset);
 
-   triangles.emplace_back(0, 1, 5);
-   triangles.emplace_back(0, 5, 4);
-   triangles.emplace_back(0, 2, 3);
-   triangles.emplace_back(0, 3, 1);
-   triangles.emplace_back(0, 4, 2);
-   triangles.emplace_back(1, 3, 7);
+   triangles.emplace_back(5, 1, 0);
+   triangles.emplace_back(4, 5, 0);
+   triangles.emplace_back(3, 2, 0);
+   triangles.emplace_back(1, 3, 0);
+   triangles.emplace_back(2, 4, 0);
+   triangles.emplace_back(7, 3, 1);
    triangles.emplace_back(1, 5, 7);
-   triangles.emplace_back(6, 5, 7);
-   triangles.emplace_back(6, 4, 5);
-   triangles.emplace_back(6, 2, 4);
+   triangles.emplace_back(7, 5, 6);
+   triangles.emplace_back(5, 4, 6);
+   triangles.emplace_back(4, 2, 6);
    triangles.emplace_back(6, 2, 3);
-   triangles.emplace_back(6, 7, 3);
+   triangles.emplace_back(3, 7, 6);
 }
 
