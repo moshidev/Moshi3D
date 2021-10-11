5b3cca7eda4ff4cb4e1b5b53e0e0ee3ea5935f91
Daniel P <46326833+moshidev@users.noreply.github.com>
2021-10-11 13:03:01 +0200

    fix Tetraedro constructor

--- tetraedro.cc
+++ tetraedro.cc
@@ -4,17 +4,17 @@
 
 Tetraedro::Tetraedro(float l)
 {
-    const float  x_offset = l / 2;
-    const float  y_offset = sqrt(5*l*l/4) / 2;
-    const float& z_offset = y_offset;
+    const float x_offset = l / 2;
+    const float y_offset = sqrt(9*l*l/16) / 2;
+    const float z_offset = sqrt(3*l*l/4) / 2;
 
     vertices.emplace_back(-x_offset, -y_offset, -z_offset);
     vertices.emplace_back( x_offset, -y_offset, -z_offset);
     vertices.emplace_back(        0, -y_offset,  z_offset);
     vertices.emplace_back(        0,  y_offset,         0);
 
-    indices.emplace_back(0, 3, 1);
-    indices.emplace_back(0, 2, 3);
-    indices.emplace_back(2, 1, 3);
-    indices.emplace_back(0, 1, 2);
+    indices.emplace_back(1, 2, 0);
+    indices.emplace_back(1, 0, 3);
+    indices.emplace_back(2, 3, 0);
+    indices.emplace_back(3, 2, 1);
 }
