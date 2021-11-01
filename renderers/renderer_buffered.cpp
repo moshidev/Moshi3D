ef8f517bf15250f4ca38469e9263ae97cd049838
Daniel P <46326833+moshidev@users.noreply.github.com>
2021-11-01 16:56:44 +0100

    feat: render color by polygon mode type

--- renderers/renderer_buffered.cpp
+++ renderers/renderer_buffered.cpp
@@ -6,22 +6,36 @@
 #include "renderer_buffered.h"
 #include "mesh.h"
 #include "_aux.h"
-#include "vertex_buffer.h"
-#include "index_buffer.h"
 
-void set_vertex_pointer(const VertexBuffer& vb) {
+static void set_vertex_pointer(const VertexBuffer& vb) {
     vb.bind();
     glVertexPointer(3, GL_FLOAT, 0, 0);
     vb.unbind();
 }
 
-void set_color_pointer(GLuint color_vbo_id) {
-    glBindBuffer(GL_ARRAY_BUFFER, color_vbo_id);
-    glColorPointer(3, GL_FLOAT, 0, 0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
+static void set_color_pointer(Mesh3D& m, int mode) {
+    const VertexBuffer* colorvb {nullptr};
+    switch (mode)
+    {
+    case GL_FILL:
+        colorvb = &m.get_color_solid_VBO();
+        break;  
+    case GL_LINE:
+        colorvb = &m.get_color_line_VBO();
+        break;
+    case GL_POINT:
+        colorvb = &m.get_color_point_VBO();
+        break;
+    }
+
+    if (colorvb) {
+        colorvb->bind();
+        glColorPointer(3, GL_FLOAT, 0, 0);
+        colorvb->unbind();
+    }
 }
 
-void draw_elements_from_indices(const IndexBuffer& ib, GLsizei count) {
+static void draw_elements_from_indices(const IndexBuffer& ib, GLsizei count) {
     ib.bind();
     glDrawElements(GL_TRIANGLES, count, GL_UNSIGNED_INT, 0);
     ib.unbind();
@@ -31,17 +45,23 @@ void RendererBuffered::render(Mesh3D& m) const {
     const std::set<int>& polygon_modes = m.get_polygon_modes();
     
     glEnable(GL_CULL_FACE);
+    glEnable(GL_POLYGON_OFFSET_LINE);
+    glPolygonOffset(-25.0, 12.5);
+
     glEnableClientState(GL_VERTEX_ARRAY);
     glEnableClientState(GL_COLOR_ARRAY);
 
     set_vertex_pointer(m.get_vertices_VBO());
-    set_color_pointer(m.get_color_VBO_id());
 
     for (auto& mode : polygon_modes) {
+        set_color_pointer(m, mode);
         glPolygonMode(GL_FRONT_AND_BACK, mode);
-        draw_elements_from_indices(m.get_indices_VBO(), m.get_indices_size());
+        draw_elements_from_indices(m.get_indices_VBO(), m.get_indices_count());
     }
 
     glDisableClientState(GL_COLOR_ARRAY);
     glDisableClientState(GL_VERTEX_ARRAY);
+
+    glDisable(GL_POLYGON_OFFSET_LINE);
+    glDisable(GL_CULL_FACE);
 }
--- renderers/renderer_immediate.cpp
+++ renderers/renderer_immediate.cpp
@@ -7,20 +7,47 @@
 #include "mesh.h"
 #include "_aux.h"
 
+static void set_color_pointer(Mesh3D& m, int mode) {
+    const GLvoid* colorvb {nullptr};
+    switch (mode)
+    {
+    case GL_FILL:
+        colorvb = m.get_color_solid_data();
+        break;  
+    case GL_LINE:
+        colorvb = m.get_color_line_data();
+        break;
+    case GL_POINT:
+        colorvb = m.get_color_point_data();
+        break;
+    }
+
+    if (colorvb) {
+        glColorPointer(3, GL_FLOAT, 0, colorvb);
+    }
+}
+
 void RendererImmediate::render(Mesh3D& m) const {
     const std::set<int>& polygon_modes = m.get_polygon_modes();
     
     glEnable(GL_CULL_FACE);
+    glEnable(GL_POLYGON_OFFSET_LINE);
+    glPolygonOffset(-25.0, 12.5);
+
     glEnableClientState(GL_VERTEX_ARRAY);
-    glVertexPointer(3, GL_FLOAT, 0, m.get_vertices_data());
     glEnableClientState(GL_COLOR_ARRAY);
-    glColorPointer(3, GL_FLOAT, 0, m.get_color_data());
+
+    glVertexPointer(3, GL_FLOAT, 0, m.get_vertices_data());
 
     for (auto& mode : polygon_modes) {
+        set_color_pointer(m, mode);
         glPolygonMode(GL_FRONT_AND_BACK, mode);
-        glDrawElements(GL_TRIANGLES, m.get_indices_size(), GL_UNSIGNED_INT, m.get_indices_data());
+        glDrawElements(GL_TRIANGLES, m.get_indices_count(), GL_UNSIGNED_INT, m.get_indices_data());
     }
 
     glDisableClientState(GL_COLOR_ARRAY);
     glDisableClientState(GL_VERTEX_ARRAY);
+
+    glDisable(GL_POLYGON_OFFSET_LINE);
+    glDisable(GL_CULL_FACE);
 }
