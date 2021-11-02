1a32bae26d7166221ff37574cfe34b673fc03045
Daniel P <46326833+moshidev@users.noreply.github.com>
2021-11-02 21:08:37 +0100

    feat: implement class Cone

---
+++ include/cone.h
@@ -0,0 +1,16 @@
+/**
+ * 2021 © Daniel Pedrosa Montes <pedrosam.sh@gmail.com>
+ * GPLv3 License.
+ */
+
+#ifndef MOSHI3D_CONE_H_
+#define MOSHI3D_CONE_H_
+
+#include "revolution_object.h"
+
+class Cone : public RevolutionObject {
+public:
+    Cone(int num_vertices_side_face, int num_instances, float height, float radius, bool make_north_cover=true);
+};
+
+#endif /* MOSHI3D_CONE_H_ */
