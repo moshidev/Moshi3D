1c7bfea626678482a51db6adcb34a0fa8ec9b053
Daniel P <46326833+moshidev@users.noreply.github.com>
2021-11-06 18:29:47 +0100

    BREAKING CHANGE: redefine Mesh3D methods and internal representation

--- include/mesh.h
+++ include/mesh.h
@@ -7,6 +7,7 @@
 #define MOSHI3D_MESH3D_H_
 
 #include <set>
+#include <list>
 #include "_aux.h"
 #include "vertex_buffer.h"
 #include "index_buffer.h"
@@ -15,56 +16,123 @@ class Renderer;
 
 class Mesh3D {
   public:
+    class BufferedData;
+    class RawData;
+    
     Mesh3D();
     virtual ~Mesh3D() = 0;
 
     virtual void draw(const Renderer& r);
 
-    inline void clear_polygon_modes(void) { polygon_modes.clear(); }
-    inline void enable_polygon_modes(int mode) { polygon_modes.insert(mode); }
-    inline void disable_polygon_modes(int mode) { polygon_modes.erase(mode); }
+    /* Permite la visualización simultánea de cualquier combinación que se quiera de GL_FILL, GL_LINE y GL_POINT*/
+    /* Si se selecciona GL_FILL y el modo ajedrez está activado lo desactiva */
+    void clear_polygon_modes(void);
+    void enable_polygon_modes(int mode);
+    void disable_polygon_modes(int mode);
     inline const std::set<int>& get_polygon_modes(void) const { return polygon_modes; }
 
-    inline void set_chess_enabled(bool b) { chess_enabled = b; }
-    inline bool get_chess_enabled(void) const { return chess_enabled; }
-    inline std::pair<Tupla3f,Tupla3f> get_chess_colors(void) const { return {{1.0, 0.0, 1.0}, {0.0, 0.125, 1.0}}; }
+    /* Habilita o deshabilita la visualización del modo ajedrez. Si GL_FILL está activado lo desactiva. */
+    void enable_chess_mode(bool b);
+    inline bool is_chess_mode_enabled(void) const { return chess_enabled; }
+
+    /* Métodos get para obtener la información a renderizar */
+    inline const std::list<BufferedData>& get_buffer_data_list(void) { return current_buffer_data_list; }
+    inline const std::list<RawData>& get_raw_data_list(void) const { return current_raw_data_list; }
 
   protected:
+    /* Inicializa los distintos vectores de colores a su valor por defecto */
     void init_color(unsigned n_vertices);
-    void calcular_normales(); // calcula tabla de normales de vértices (práctica 3)
+
+    /* Limpia e establece (actualiza) la lista de información a renderizar */
+    virtual void make_data_lists(void);
+
+    std::list<BufferedData> get_chess_buffer_list(VertexBuffer& vertices, IndexBuffer& indices);
+    std::list<RawData> get_chess_raw_list(void) const;
+
+    VertexBuffer& get_vertices_VBO(void);
+    IndexBuffer& get_indices_VBO(void);
+    VertexBuffer& get_color_fill_VBO(void);
+    VertexBuffer& get_color_line_VBO(void);
+    VertexBuffer& get_color_point_VBO(void);
+
+    void init_vertex_buffer(VertexBuffer*& vb, const std::vector<Tupla3f>& v);
+    void init_index_buffer(IndexBuffer*& ib, const std::vector<Tupla3u>& v);
+    void update_vertex_buffer(VertexBuffer*& vb, GLsizeiptr size, const GLvoid* data);
+    void update_index_buffer(IndexBuffer*& ib, GLsizei count, const GLvoid* data);
+
+    void calcular_normales(void); // calcula tabla de normales de vértices (práctica 3)
+
+    bool chess_enabled;
+    std::set<int> polygon_modes;
 
     std::vector<Tupla3f> vertices;
     std::vector<Tupla3u> indices;
-    std::vector<Tupla3f> color_solid;
+
+    std::list<BufferedData> current_buffer_data_list;
+    std::list<RawData> current_raw_data_list;
 
   private:
+    std::vector<Tupla3f> color_fill;
     std::vector<Tupla3f> color_line;
     std::vector<Tupla3f> color_point;
+    std::vector<Tupla3f> color_chess_a;
+    std::vector<Tupla3f> color_chess_b;
 
     VertexBuffer* vertices_VBO {nullptr};
-    VertexBuffer* color_solid_VBO {nullptr};
+    IndexBuffer* indices_VBO {nullptr};
+    VertexBuffer* color_fill_VBO {nullptr};
     VertexBuffer* color_line_VBO {nullptr};
     VertexBuffer* color_point_VBO {nullptr};
-    IndexBuffer* indices_VBO {nullptr};
+    VertexBuffer* color_chess_a_VBO {nullptr};
+    VertexBuffer* color_chess_b_VBO {nullptr};
+};
 
-    std::set<int> polygon_modes;
+class Mesh3D::BufferedData {
+    VertexBuffer& vertices;
+    IndexBuffer& face_indices;
+    VertexBuffer& color;
+    unsigned indices_offset;
+    unsigned indices_count;
+    int polygon_mode;
 
-    bool chess_enabled;
+public:
+    BufferedData(VertexBuffer& vertices, IndexBuffer& faces, VertexBuffer& color, int polygon_mode)
+        :vertices{vertices}, face_indices{faces}, color{color},
+         indices_offset{0}, indices_count{faces.get_num_indices()},
+         polygon_mode{polygon_mode}
+    {}
 
-  public:
-    inline const GLvoid* get_vertices_data(void) const { return vertices.data(); }
-    inline const GLvoid* get_color_solid_data(void) const { return color_solid.data(); }
-    inline const GLvoid* get_color_line_data(void) const { return color_line.data(); }
-    inline const GLvoid* get_color_point_data(void) const { return color_point.data(); }
-    inline const GLvoid* get_indices_data(void) const { return indices.data(); }
-    inline GLsizei get_indices_count(void) const { return 3*indices.size(); }
+    inline const VertexBuffer& get_vertices(void) const { return vertices; }
 
-    const VertexBuffer& get_vertices_VBO(void);
-    const VertexBuffer& get_color_solid_VBO(void);
-    const VertexBuffer& get_color_line_VBO(void);
-    const VertexBuffer& get_color_point_VBO(void);
-    const IndexBuffer& get_indices_VBO(void);
+    inline const IndexBuffer& get_indices(void) const { return face_indices; }
+    inline unsigned get_indices_offset(void) const { return indices_offset; }
+    inline unsigned get_indices_count(void) const { return indices_count; }
+    inline void set_face_indices_offset(unsigned offset, unsigned count) { indices_offset = offset; indices_count = count; }
 
+    inline const VertexBuffer& get_color(void) const { return color; }
+
+    inline int get_polygon_mode(void) const { return polygon_mode; }
+};
+
+class Mesh3D::RawData {
+    std::vector<Tupla3f>& vertices_data;
+    std::vector<Tupla3f>& color_data;
+    std::vector<Tupla3u>& face_indices_data;
+    unsigned indices_offset;
+    unsigned indices_size;
+    int polygon_mode;
+
+public:
+    RawData(std::vector<Tupla3f>& vertices, std::vector<Tupla3f>& color, std::vector<Tupla3u>& faces, int polygon_mode);
+    int get_polygon_mode(void);
+
+    inline const std::vector<Tupla3f>& get_vertices_data(void) const {return vertices_data; }
+
+    inline const std::vector<Tupla3u>& get_indices_data(void) const { return face_indices_data; };
+    inline unsigned get_indices_offset(void) const { return indices_offset; }
+    inline unsigned get_indices_size(void) const { return indices_size; }
+
+    inline const std::vector<Tupla3f>& get_color_data(void) const { return color_data; }
 };
 
 #endif /* MOSHI3D_MESH3D_H_ */
--- primitives/mesh.cpp
+++ primitives/mesh.cpp
@@ -8,16 +8,14 @@
 #include "_aux.h"
 #include <iostream>
 
-Mesh3D::Mesh3D() {
-    enable_polygon_modes(GL_FILL);
-}
+Mesh3D::Mesh3D() {}
 
 Mesh3D::~Mesh3D() {
     if (vertices_VBO) {
         delete vertices_VBO;
     }
-    if (color_solid_VBO) {
-        delete color_solid_VBO;
+    if (color_fill_VBO) {
+        delete color_fill_VBO;
     }
     if (color_line_VBO) {
         delete color_line_VBO;
@@ -25,52 +23,149 @@ Mesh3D::~Mesh3D() {
     if (color_point_VBO) {
         delete color_point_VBO;
     }
+    if (color_chess_a_VBO) {
+        delete color_chess_a_VBO;
+    }
+    if (color_chess_b_VBO) {
+        delete color_chess_b_VBO;
+    }
     if (indices_VBO) {
         delete indices_VBO;
     }
-}
-
-void Mesh3D::init_color(unsigned n_vertices) {
-    color_solid.assign(n_vertices, {0.25, 0.5, 0.75});
-    color_line.assign(n_vertices, {1.0, 0.0, 0.0});
-    color_point.assign(n_vertices, {0.0, 1.0, 0.0});
 }
 
 void Mesh3D::draw(const Renderer& r) {
     r.render(*this);
 }
 
-const VertexBuffer& Mesh3D::get_vertices_VBO(void) {
-    if (vertices_VBO == nullptr) {
-        vertices_VBO = new VertexBuffer(sizeof(Tupla3f)*vertices.size(), vertices.data());
+void Mesh3D::make_data_lists(void) {
+    current_buffer_data_list.clear();
+
+    VertexBuffer& v = get_vertices_VBO();
+    IndexBuffer& i = get_indices_VBO();
+    const auto& it = current_buffer_data_list.begin();
+
+    if (chess_enabled) {
+        current_buffer_data_list.splice(it, get_chess_buffer_list(v, i));
     }
+
+    for (const auto& m : polygon_modes) {
+        switch (m)
+        {
+        case GL_FILL:
+            current_buffer_data_list.emplace_back(v, i, get_color_fill_VBO(), GL_FILL);
+            break;
+        case GL_LINE:
+            current_buffer_data_list.emplace_back(v, i, get_color_line_VBO(), GL_LINE);
+            break;
+        case GL_POINT:
+            current_buffer_data_list.emplace_back(v, i, get_color_point_VBO(), GL_POINT);
+        }
+    }
+}
+
+void Mesh3D::clear_polygon_modes(void) {
+    polygon_modes.clear();
+    make_data_lists();
+}
+
+void Mesh3D::enable_polygon_modes(int mode) {
+    if (mode == GL_FILL && chess_enabled) {
+        chess_enabled = false;
+    }
+    polygon_modes.insert(mode);
+    make_data_lists();
+}
+
+void Mesh3D::disable_polygon_modes(int mode) {
+    polygon_modes.erase(mode);
+    make_data_lists();
+}
+
+void Mesh3D::enable_chess_mode(bool b) {
+    if (b) {
+        polygon_modes.erase(GL_FILL);
+    }
+    chess_enabled = b;
+    make_data_lists();
+}
+
+void Mesh3D::init_color(unsigned n_vertices) {
+    color_fill.assign(n_vertices, {0.25, 0.5, 0.75});
+    color_line.assign(n_vertices, {1.0, 0.0, 0.0});
+    color_point.assign(n_vertices, {0.0, 1.0, 0.0});
+    color_chess_a.assign(n_vertices, {0.75, 0.25, 0.0});
+    color_chess_b.assign(n_vertices, {0.25, 0.75, 0.0});
+}
+
+void Mesh3D::init_vertex_buffer(VertexBuffer*& vb, const std::vector<Tupla3f>& v) {
+    if (!vb) {
+        update_vertex_buffer(vb, v.size()*sizeof(Tupla3f), v.data());
+    }
+}
+
+void Mesh3D::init_index_buffer(IndexBuffer*& ib, const std::vector<Tupla3u>& v) {
+    if (!ib) {
+        update_index_buffer(ib, v.size()*3, v.data());
+    }
+}
+
+void Mesh3D::update_vertex_buffer(VertexBuffer*& vb, GLsizeiptr size, const GLvoid* data) {
+    if (vb) {
+        delete vb;
+    }
+    vb = new VertexBuffer(size, data);
+}
+
+void Mesh3D::update_index_buffer(IndexBuffer*& ib, GLsizei count, const GLvoid* data) {
+    if (ib) {
+        delete ib;
+    }
+    ib = new IndexBuffer(count, data);
+}
+
+VertexBuffer& Mesh3D::get_vertices_VBO(void) {
+    init_vertex_buffer(vertices_VBO, vertices);
     return *vertices_VBO;
 }
 
-const VertexBuffer& Mesh3D::get_color_solid_VBO(void) {
-    if (color_solid_VBO == nullptr) {
-        color_solid_VBO = new VertexBuffer(sizeof(Tupla3f)*color_solid.size(), color_solid.data());
-    }
-    return *color_solid_VBO;
+IndexBuffer& Mesh3D::get_indices_VBO(void) {
+    init_index_buffer(indices_VBO, indices);
+    return *indices_VBO;
 }
 
-const VertexBuffer& Mesh3D::get_color_line_VBO(void) {
-    if (color_line_VBO == nullptr) {
-        color_line_VBO = new VertexBuffer(sizeof(Tupla3f)*color_line.size(), color_line.data());
-    }
+VertexBuffer& Mesh3D::get_color_fill_VBO(void) {
+    init_vertex_buffer(color_fill_VBO, color_fill);
+    return *color_fill_VBO;
+}
+
+VertexBuffer& Mesh3D::get_color_line_VBO(void) {
+    init_vertex_buffer(color_line_VBO, color_line);
     return *color_line_VBO;
 }
 
-const VertexBuffer& Mesh3D::get_color_point_VBO(void) {
-    if (color_point_VBO == nullptr) {
-        color_point_VBO = new VertexBuffer(sizeof(Tupla3f)*color_point.size(), color_point.data());
-    }
+VertexBuffer& Mesh3D::get_color_point_VBO(void) {
+    init_vertex_buffer(color_point_VBO, color_point);
     return *color_point_VBO;
 }
 
-const IndexBuffer& Mesh3D::get_indices_VBO(void) {
-    if (indices_VBO == nullptr) {
-        indices_VBO = new IndexBuffer(3*indices.size(), indices.data());
-    }
-    return *indices_VBO;
+std::list<Mesh3D::BufferedData> Mesh3D::get_chess_buffer_list(VertexBuffer& vertices, IndexBuffer& indices) {
+    init_vertex_buffer(color_chess_a_VBO, color_chess_a);
+    init_vertex_buffer(color_chess_b_VBO, color_chess_b);
+
+    std::list<Mesh3D::BufferedData> l;
+    int mid = indices.get_num_indices()/2;
+    const int& count = mid;
+
+    l.emplace_back(vertices, indices, *color_chess_a_VBO, GL_FILL);
+    l.back().set_face_indices_offset(0, count);
+
+    l.emplace_back(vertices, indices, *color_chess_b_VBO, GL_FILL);
+    l.back().set_face_indices_offset(mid, count);
+
+    return l;
+}
+
+std::list<Mesh3D::RawData> Mesh3D::get_chess_raw_list(void) const {
+
 }
