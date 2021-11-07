b88070b674b91eac6dbc53b2017a04d8c50bf3b5
Daniel P <46326833+moshidev@users.noreply.github.com>
2021-11-07 17:40:04 +0100

    refactor: implement rendering using RawData list

--- renderers/renderer_immediate.cpp
+++ renderers/renderer_immediate.cpp
@@ -7,90 +7,24 @@
 #include "mesh.h"
 #include "_aux.h"
 
-/*static void set_color_pointer(Mesh3D& m, int mode) {
-    const GLvoid* colorvb {nullptr};
-    switch (mode)
-    {
-    case GL_FILL:
-        colorvb = m.get_color_solid_data();
-        break;  
-    case GL_LINE:
-        colorvb = m.get_color_line_data();
-        break;
-    case GL_POINT:
-        colorvb = m.get_color_point_data();
-        break;
-    }
-
-    if (colorvb) {
-        glColorPointer(3, GL_FLOAT, 0, colorvb);
-    }
-}
-
-static void render_chess(Mesh3D& m) {
-    const std::set<int>& polygon_modes = m.get_polygon_modes();
-
-    glEnableClientState(GL_VERTEX_ARRAY);
-    glDisableClientState(GL_COLOR_ARRAY);
-
-    glVertexPointer(3, GL_FLOAT, 0, m.get_vertices_data());
-    
-    for (auto& mode : polygon_modes) {
-        glPolygonMode(GL_FRONT_AND_BACK, mode);
-        if (mode == GL_LINE || mode == GL_POINT) {
-            if (mode == GL_LINE) {
-                glColor3f(1.0, 0.0, 0.0);
-            }
-            else {
-                glColor3f(0.0, 1.0, 0.0);
-            }
-            glDrawElements(GL_TRIANGLES, m.get_indices_count(), GL_UNSIGNED_INT, m.get_indices_data());
-        }
-        else {
-            const auto& colors = m.get_chess_colors();
-            GLsizei first_half = m.get_indices_count()/2;
-            GLsizei second_half = m.get_indices_count() - first_half;
-            glColor3fv((GLfloat*)&colors.first);
-            glDrawElements(GL_TRIANGLES, first_half, GL_UNSIGNED_INT, m.get_indices_data());
-            glColor3fv((GLfloat*)&colors.second);
-            glDrawElements(GL_TRIANGLES, second_half, GL_UNSIGNED_INT, (unsigned int*)m.get_indices_data() + first_half);
-        }
-    }
-
-
-    glDisableClientState(GL_VERTEX_ARRAY);
-}
-
-static void render_std(Mesh3D& m) {
-    const std::set<int>& polygon_modes = m.get_polygon_modes();
-    
-    glEnableClientState(GL_VERTEX_ARRAY);
-    glEnableClientState(GL_COLOR_ARRAY);
-
-    glVertexPointer(3, GL_FLOAT, 0, m.get_vertices_data());
-
-    for (auto& mode : polygon_modes) {
-        set_color_pointer(m, mode);
-        glPolygonMode(GL_FRONT_AND_BACK, mode);
-        glDrawElements(GL_TRIANGLES, m.get_indices_count(), GL_UNSIGNED_INT, m.get_indices_data());
-    }
-
-    glDisableClientState(GL_COLOR_ARRAY);
-    glDisableClientState(GL_VERTEX_ARRAY);
-}*/
-
 void RendererImmediate::render(Mesh3D& m) const {
-    /*glEnable(GL_CULL_FACE);
+    glEnable(GL_CULL_FACE);
     glEnable(GL_POLYGON_OFFSET_LINE);
     glPolygonOffset(-25.0, 12.5);
 
-    if (m.is_chess_mode_enabled()) {
-        render_chess(m);
-    }
-    else {
-        render_std(m);
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_COLOR_ARRAY);
+
+    for (const auto& r : m.get_raw_data_list()) {
+        glPolygonMode(GL_FRONT_AND_BACK, r.get_polygon_mode());
+        glVertexPointer(3, GL_FLOAT, 0, r.get_vertices_data().data());
+        glColorPointer(3, GL_FLOAT, 0, r.get_color_data().data());
+        glDrawElements(GL_TRIANGLES, r.get_indices_count(), GL_UNSIGNED_INT, r.get_indices_data().data() + r.get_indices_offset()/3);
     }
 
+    glDisableClientState(GL_VERTEX_ARRAY);
+    glDisableClientState(GL_COLOR_ARRAY);
+
     glDisable(GL_POLYGON_OFFSET_LINE);
-    glDisable(GL_CULL_FACE);*/
+    glDisable(GL_CULL_FACE);
 }
