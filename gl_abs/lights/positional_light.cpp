7501d551c7d8de88af8a51b71c6af0abebb93520
Daniel P <46326833+moshidev@users.noreply.github.com>
2021-11-18 23:24:13 +0100

    fix: bad constructor parameter constant

--- gl_abs/lights/positional_light.cpp
+++ gl_abs/lights/positional_light.cpp
@@ -1,9 +1,9 @@
 #include "positional_light.h"
 
 PositionalLight::PositionalLight(const Tupla3f& position)
-:Light{Tupla4f{position(0), position(1), position(2), 0}}
+:Light{Tupla4f{position(0), position(1), position(2), 1}}
 {   }
 
 PositionalLight::PositionalLight(const Tupla3f& position, Tupla4f color_ambient, Tupla4f color_diffuse, Tupla4f color_specular)
-:Light{Tupla4f{position(0), position(1), position(2), 0}, color_ambient, color_diffuse, color_specular}
+:Light{Tupla4f{position(0), position(1), position(2), 1}, color_ambient, color_diffuse, color_specular}
 {   }
