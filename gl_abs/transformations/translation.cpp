2deedbf55a12b5c6a38a82471657e08134a206a1
Daniel P <46326833+moshidev@users.noreply.github.com>
2021-11-29 13:01:18 +0100

    feat: implement operator{+,-} and interpolation functions

--- gl_abs/transformations/translation.cpp
+++ gl_abs/transformations/translation.cpp
@@ -1,17 +1,38 @@
 #include "translation.h"
 
 Translation::Translation(const Tupla3f& t)
-:translation{t}
+:translation_val{t}
 {   }
 
 void Translation::set(const Tupla3f& t) {
-    translation = t;
+    translation_val = t;
 }
 
 void Translation::sum(const Tupla3f& t) {
-    translation = translation + t;
+    translation_val = translation_val + t;
 }
 
 void Translation::apply(void) const {
-    glTranslatef(translation[0], translation[1], translation[2]);
+    glTranslatef(translation_val[0], translation_val[1], translation_val[2]);
+}
+
+Translation& Translation::multiply_member_data(float val) {
+    translation_val = translation_val * val;
+    return *this;
+}
+
+Translation operator+(const Translation& lt, const Translation& rt) {
+    Translation ret{lt};
+    ret.sum(rt.translation_val);
+    return ret;
+}
+
+Translation operator-(const Translation& lt, const Translation& rt) {
+    Translation ret{lt};
+    ret.sum(-rt.translation_val);
+    return ret;
+}
+
+Translation interpolation(const Translation& lt, const Translation& rt, float percentaje, const std::function<float(float)>& f) {
+    return lt + (rt-lt).multiply_member_data(f(percentaje));
 }
--- gl_abs/transformations/translation.h
+++ gl_abs/transformations/translation.h
@@ -7,9 +7,11 @@
 #define MOSHI3D_TRANSLATION_H_
 
 #include "transformation.h"
+#include <functional>
 
 class Translation : public Transformation {
-    Tupla3f translation;
+    Tupla3f translation_val;
+    Translation& multiply_member_data(float val);
 
 public:
     Translation() = delete;
@@ -19,7 +21,11 @@ public:
     void sum(const Tupla3f& t);
     virtual void apply(void) const;
 
-    inline const Tupla3f& get(void) const { return translation; }
+    inline const Tupla3f& get(void) const { return translation_val; }
+
+    friend Translation operator+(const Translation& lr, const Translation& rt);
+    friend Translation operator-(const Translation& lr, const Translation& rt);
+    friend Translation interpolation(const Translation& lr, const Translation& rt, float percentaje, const std::function<float(float)>& f);
 };
 
 #endif /* MOSHI3D_TRANSLATION_H_ */
