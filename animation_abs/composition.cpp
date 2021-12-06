582e8480f14b2ed6da1c0bd93bcf06be20b793c0
Daniel P <46326833+moshidev@users.noreply.github.com>
2021-12-06 21:29:21 +0100

    fix: apply algorithm

--- animation_abs/composition.cpp
+++ animation_abs/composition.cpp
@@ -1,23 +1,34 @@
 #include "composition.h"
 #include <queue>
+#include <stack>
 
 bool Composition::apply(iterator it, float time_point) const {
     bool visible = true;
-    while (visible && it != rooted_DAG.end()) {
-        auto it_parent = it.get_last_parent();
+    std::stack<Composition::iterator> stack;
+    stack.push(it);
+    auto it_parent = it.get_last_parent();
+    while (it_parent != rooted_DAG.end()) {
+        stack.push(it_parent);
+        it_parent = it_parent.get_last_parent();
+    }
+    float init_time = 0;
+    while (visible && !stack.empty()) {
+        auto& it = stack.top();
         const auto& atributes = (*it).atributes;
         const auto& animation = (*it).animation;
+
+        init_time = std::max(init_time, atributes.init_time);
         visible = atributes.is_visible(time_point);
         if (visible) {
             atributes.location.apply();
 
             float local_time_point = time_point;
             if (atributes.loop) {
-                local_time_point = fmod(local_time_point+atributes.init_time, atributes.duration);
+                local_time_point = fmod(local_time_point+init_time, atributes.duration);
             }
             animation.apply(local_time_point);
         }
-        it = it_parent;
+        stack.pop();
     }
     return visible;
 }
@@ -26,11 +37,10 @@ void Composition::draw(const Renderer& r, float time_point) const {
     std::cout << "tp: " << time_point << std::endl;
     auto it = rooted_DAG.begin();
     while (it != rooted_DAG.end()) {
-        const auto& obj_v {(*it).get_objects()};
-        for (const auto& obj : obj_v) {
+        for (const auto& obj : (*it).get_objects()) {
             Transformation::push_matrix();
-            obj.atributes.location.apply();
             if (apply(it, time_point)) {
+                obj.atributes.location.apply();
                 obj.renderizable_ptr->draw(r);
             }
             Transformation::pop_matrix();
@@ -63,9 +73,3 @@ Composition::iterator Composition::emplace_child(const iterator& parent, const C
 void Composition::add_child_to_parent(const iterator& parent, const iterator& child) {
     rooted_DAG.add_child(parent, child);
 }
-
-//<--- nos falta un ".apply" o algo parecido;
-            //aquí tendríamos que manejar de algua forma el "si es o no visible", si está en loop o no...
-            // hay que limitar la asignación de valores a tiempo inicio y tiempo de duración según en el ámbito
-            // donde se modifiquen/pertenezcan los atributos
-            // Algo así como una estructura de datos temporal que tenga constancia de cada nivel del árbol superior  para el nodo actual;
