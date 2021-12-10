48443428813fb0a99be82f7a7fbcb85ff410f373
Daniel P <46326833+moshidev@users.noreply.github.com>
2021-12-10 19:25:01 +0100

    refactor: reimplement and change class Animation interface

--- animation_abs/animation.cpp
+++ animation_abs/animation.cpp
@@ -1,6 +1,30 @@
 #include "animation.h"
 
-float Animation::calc_max_time_point(void) const {
+Animation::Animation() {
+    set_percentaje(0);
+}
+
+Animation::Animation(float ini_percentaje) {
+    set_percentaje(ini_percentaje);
+}
+
+Animation::Animation(const Animation& a) {
+    set_percentaje(a.percentaje);
+}
+
+void Animation::apply(void) const {
+    if (!translation_tt.empty()) {
+        translation_tt.get_transformation(percentaje).apply();
+    }
+    if (!scaling_tt.empty()) {
+        scaling_tt.get_transformation(percentaje).apply();
+    }
+    if (!rotation_tt.empty()) {
+        rotation_tt.get_transformation(percentaje).apply();
+    }
+}
+
+float Animation::get_lenght(void) const {
     float max = 0;
     if (!translation_tt.empty()) {
         max = std::max(translation_tt.rbegin()->first, max);
@@ -14,18 +38,49 @@ float Animation::calc_max_time_point(void) const {
     return max;
 }
 
-void Animation::apply(float time_point) const {
-    if (loop) {
-        float max = calc_max_time_point();
-        time_point = fmod(time_point, max);
+bool Animation::set_percentaje(float p) {
+    percentaje = p > 1 ? 1 : p;
+    percentaje = p < 0 ? 0 : p;
+    last_time_point = std::chrono::steady_clock::now();
+    return p > 1 || p < 0;
+}
+
+bool Animation::set_percentaje_loop(float p) {
+    if (p < 0 || p > 1) {
+        p = std::fmod(p, 1);
+        p = p < 0 ? p+1 : p;
     }
-    if (!translation_tt.empty()) {
-        translation_tt.get_transformation(time_point).apply();
+    percentaje = p;
+    last_time_point = std::chrono::steady_clock::now();
+    return p > 1 || p < 0;
+}
+
+float Animation::calc_animation_percentaje_step_size(float time_elapsed) const {
+    float len = get_lenght();
+    if (len) {
+        return time_elapsed * anim_factor * 1/get_lenght();
     }
-    if (!scaling_tt.empty()) {
-        scaling_tt.get_transformation(time_point).apply();
+    else {
+        return 0;
     }
-    if (!rotation_tt.empty()) {
-        rotation_tt.get_transformation(time_point).apply();
-    }
+}
+
+bool Animation::increment_anim(float time_elapsed) {
+    return set_percentaje(get_percentaje() + calc_animation_percentaje_step_size(time_elapsed));
+}
+
+bool Animation::increment_anim_loop(float time_elapsed) {
+    return set_percentaje_loop(get_percentaje() + calc_animation_percentaje_step_size(time_elapsed));
+}
+
+bool Animation::increment_anim(void) {
+    auto tp = std::chrono::steady_clock::now();
+    float time_elapsed = std::chrono::duration<float>(tp-last_time_point).count();
+    return increment_anim(time_elapsed);
+}
+
+bool Animation::increment_anim_loop(void) {
+    auto tp = std::chrono::steady_clock::now();
+    float time_elapsed = std::chrono::duration<float>(tp-last_time_point).count();
+    return increment_anim_loop(time_elapsed);
 }
--- animation_abs/animation.h
+++ animation_abs/animation.h
@@ -11,18 +11,40 @@
 #include "scaling.h"
 #include "translation.h"
 #include "transformation_timeline.hpp"
+#include <chrono>
 #include <vector>
 
 class Animation {
-    float calc_max_time_point(void) const;
+    float percentaje {0};
+    float anim_factor {1};
+    std::chrono::time_point<std::chrono::steady_clock> last_time_point;
     
+    float calc_animation_percentaje_step_size(float time_elapsed) const;
+
 public:
     TransformationTimeline<Translation> translation_tt;
     TransformationTimeline<Scaling> scaling_tt;
     TransformationTimeline<Rotation> rotation_tt;
-    bool loop {false};
+
+    Animation();
+    Animation(float ini_percentaje);
+    Animation(const Animation& a);
     
-    void apply(float time_point) const;
+    void apply(void) const;
+    float get_lenght(void) const;
+    inline float get_percentaje(void) const { return percentaje; }
+    
+    bool set_percentaje(float percentaje);
+    bool set_percentaje_loop(float percentaje);
+
+    inline float get_speed_factor(void) const { return anim_factor; }
+    inline void set_speed_factor(float f) { anim_factor = f; }
+
+    bool increment_anim(float time_elapsed);
+    bool increment_anim_loop(float time_elapsed);
+
+    bool increment_anim(void);
+    bool increment_anim_loop(void);
 };
 
 #endif /* MOSHI3D_ANIMATION_H_ */
