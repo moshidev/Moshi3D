3e916583a53715c429d80e4d28bc3a4507f05c9c
Daniel P <46326833+moshidev@users.noreply.github.com>
2022-01-06 14:14:19 +0100

    feat: make accesible camera selector menu

--- escena/menus/visualization_mode_selection.h
+++ escena/menus/visualization_mode_selection.h
@@ -7,17 +7,19 @@
 #define MOSHI3D_MENUS_VISUALIZATION_MODE_SELECTION_H_
 
 #include "visualization_ilumination.h"
+#include "visualization_cameras.h"
 #include "menu.h"
 
 class VisualizationModeSelection : public Menu {
     VisualizationModeSelectionIlumination ilumination;
+    VisualizationModeSelectionCameras cameras;
 
 protected:
     virtual bool handler(Escena& e, unsigned char tecla, int x, int y);
 
 public:
     VisualizationModeSelection(Menu* parent)
-    : Menu{parent}, ilumination{this}
+    : Menu{parent}, ilumination{this}, cameras{this}
     {   }
 };
 
